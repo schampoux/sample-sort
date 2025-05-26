2a020e996c8845e9795f669785c844371a672d8f
schampoux <sergiochampoux@gmail.com>
2025-05-25 22:48:37 -0400

    create_output_dict for storing classified sounds

--- src/sorter/utils.py
+++ src/sorter/utils.py
@@ -1,6 +1,8 @@
 import yaml 
 import re 
 from pathlib import Path 
+from collections import defaultdict
+from typing import DefaultDict, List 
 
 def load_rules(path='./rules.yaml'):
     with open(path, 'r') as f: 
@@ -10,6 +12,18 @@ def load_rules(path='./rules.yaml'):
         for rule in config['rules']
     ]
     return compiled_rules
+
+def create_output_dict() -> DefaultDict[str, List]:
+    compiled_rules = load_rules()
+    labels = []
+    for i in compiled_rules:
+        labels.append(str(i[-1]))
+
+    labels
+    dct = defaultdict(list)
+    for i in labels:
+        dct[i]=[]
+    return dct 
 
 def fill_path(path: str):
     path.replace(' ', '-')
